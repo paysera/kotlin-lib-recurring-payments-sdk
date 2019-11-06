17544c3f325f7422e4b4137408228107b0fdda44
Gilbert Parreno <gilbertparreno95@gmail.com>
2019-11-06 06:20:14 +0200

    fixed url paths and make some properties optional

--- src/main/kotlin/com/paysera/lib/recurring_payments/entities/Recurrence.kt
+++ src/main/kotlin/com/paysera/lib/recurring_payments/entities/Recurrence.kt
@@ -1,17 +1,21 @@
 package com.paysera.lib.recurring_payments.entities
 
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.JsonAdapter
 import com.google.gson.annotations.SerializedName
 import com.paysera.lib.accounts.entities.transfers.Transfer
 import java.util.*
 
 data class Recurrence(
-    var id: Int,
-    var status: String,
+    var id: Int? = null,
+    var status: String? = null,
     @SerializedName("recurrence_type")
     var type: String,
-    var transferType: String,
-    var createdAt: Date,
+    var transferType: String? = null,
+    var createdAt: Date? = null,
+    @SerializedName("transfer_data")
+//    @JsonAdapter(TransferDataAdapter::class)
     var transfer: Transfer,
-    var dayOfWeek: Int?,
-    var dayOfMonth: Int?
+    var dayOfWeek: Int? = null,
+    var dayOfMonth: Int? = null
 )
--- src/main/kotlin/com/paysera/lib/recurring_payments/retrofit/NetworkApiFactory.kt
+++ src/main/kotlin/com/paysera/lib/recurring_payments/retrofit/NetworkApiFactory.kt
@@ -1,15 +1,39 @@
 package com.paysera.lib.recurring_payments.retrofit
 
+import com.google.gson.ExclusionStrategy
+import com.google.gson.FieldAttributes
+import com.google.gson.GsonBuilder
 import com.paysera.lib.common.entities.ApiCredentials
 import com.paysera.lib.common.interfaces.TokenRefresherInterface
 import com.paysera.lib.common.retrofit.BaseApiFactory
 import com.paysera.lib.recurring_payments.clients.RecurringPaymentsApiClient
+import retrofit2.converter.gson.GsonConverterFactory
 
 class NetworkApiFactory(credentials: ApiCredentials) : BaseApiFactory<RecurringPaymentsApiClient>(credentials) {
 
     override fun createClient(baseUrl: String, tokenRefresher: TokenRefresherInterface?): RecurringPaymentsApiClient {
         return RecurringPaymentsApiClient(
-            createRetrofit(baseUrl, tokenRefresher).create(NetworkApiClient::class.java)
+            createRetrofit(baseUrl, tokenRefresher).also {
+                val exclusionStrategy = object : ExclusionStrategy {
+                    val fieldsToExclude = listOf("isPayzaAccount", "isBankAccount", "isWebmoneyAccount", "isTaxAccount", "isPayseraAccount")
+                    override fun shouldSkipClass(clazz: Class<*>?): Boolean {
+                        return false
+                    }
+
+                    override fun shouldSkipField(f: FieldAttributes?): Boolean {
+                        fieldsToExclude.firstOrNull {
+                            it.equals(f?.name, true)
+                        }?.let {
+                            println("gibbs not skipped: $it")
+                            return true
+                        }
+
+                        println("gibbs not skipped: ${f?.name}")
+                        return false
+                    }
+                }
+                it.newBuilder().addConverterFactory(GsonConverterFactory.create(GsonBuilder().setExclusionStrategies(exclusionStrategy).create())).build()
+            }.create(NetworkApiClient::class.java)
         )
     }
 }
