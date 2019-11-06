bcb06f5798afd68fd8cdd8f8c725b058c2b15794
Gilbert Parreno <gilbertparreno95@gmail.com>
2019-11-06 09:40:13 +0200

    Revert "fixed url paths and make some properties optional"
    This reverts commit e1210ffd2fa022c434078805f59ce8990111b154.

--- src/main/kotlin/com/paysera/lib/recurring_payments/entities/Recurrence.kt
+++ src/main/kotlin/com/paysera/lib/recurring_payments/entities/Recurrence.kt
@@ -1,21 +1,17 @@
 package com.paysera.lib.recurring_payments.entities
 
-import com.google.gson.annotations.Expose
-import com.google.gson.annotations.JsonAdapter
 import com.google.gson.annotations.SerializedName
 import com.paysera.lib.accounts.entities.transfers.Transfer
 import java.util.*
 
 data class Recurrence(
-    var id: Int? = null,
-    var status: String? = null,
+    var id: Int,
+    var status: String,
     @SerializedName("recurrence_type")
     var type: String,
-    var transferType: String? = null,
-    var createdAt: Date? = null,
-    @SerializedName("transfer_data")
-//    @JsonAdapter(TransferDataAdapter::class)
+    var transferType: String,
+    var createdAt: Date,
     var transfer: Transfer,
-    var dayOfWeek: Int? = null,
-    var dayOfMonth: Int? = null
+    var dayOfWeek: Int?,
+    var dayOfMonth: Int?
 )
