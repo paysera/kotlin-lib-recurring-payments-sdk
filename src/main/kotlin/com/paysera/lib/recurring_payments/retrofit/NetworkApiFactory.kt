518fa103927a906e465d356c3100d996dfadd901
Edgar Žigis <tranced.freak@gmail.com>
2020-08-20 09:34:16 +0300

    Implement logging interface

--- src/main/kotlin/com/paysera/lib/recurring_payments/retrofit/NetworkApiFactory.kt
+++ src/main/kotlin/com/paysera/lib/recurring_payments/retrofit/NetworkApiFactory.kt
@@ -1,6 +1,7 @@
 package com.paysera.lib.recurring_payments.retrofit
 
 import com.paysera.lib.common.entities.ApiCredentials
+import com.paysera.lib.common.interfaces.ErrorLoggerInterface
 import com.paysera.lib.common.interfaces.TokenRefresherInterface
 import com.paysera.lib.common.retrofit.BaseApiFactory
 import com.paysera.lib.recurring_payments.clients.RecurringPaymentsApiClient
@@ -10,12 +11,14 @@ class NetworkApiFactory(
     userAgent: String?,
     credentials: ApiCredentials,
     timeout: Long? = null,
-    httpLoggingInterceptorLevel: HttpLoggingInterceptor.Level = HttpLoggingInterceptor.Level.BASIC
+    httpLoggingInterceptorLevel: HttpLoggingInterceptor.Level = HttpLoggingInterceptor.Level.BASIC,
+    errorLoggerInterface: ErrorLoggerInterface
 ) : BaseApiFactory<RecurringPaymentsApiClient>(
     userAgent,
     credentials,
     timeout,
-    httpLoggingInterceptorLevel
+    httpLoggingInterceptorLevel,
+    errorLoggerInterface
 ) {
     override val baseUrl = "https://recurring-payments.paysera.com/recurrence/rest/v1/"
     override val certifiedHosts = listOf("recurring-payments.paysera.com")
