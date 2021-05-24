4929bca8a11edef5a7f5c28da9cb98e85b238a86
Gilbert Parreno <gilbertparreno95@gmail.com>
2021-05-24 15:57:53 +0800

    migrate to kotlin-dsl and set up for maven central

---
+++ buildSrc/src/main/kotlin/ApplicationConfiguration.kt
@@ -0,0 +1,10 @@
+object ApplicationConfiguration {
+    const val artifactId = "kotlin-lib-recurring-payments-sdk"
+    const val groupId = "com.paysera.lib"
+    const val version = "1.8.1"
+    const val name = "Kotlin Library Recurring Payments SDK"
+    const val url = "https://www.paysera.com/v2/en-GB/index"
+    const val description = "Paysera: Kotlin Recurring Payments SDK Library"
+    const val releaseRepositoryUrl = "https://s01.oss.sonatype.org/content/repositories/releases/"
+    const val snapshotRepositoryUrl = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
+}
