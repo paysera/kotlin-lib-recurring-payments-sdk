d324fb75e5023ba176efbba3b459f8bc145ba012
Gilbert Parreno <gilbertparreno95@gmail.com>
2021-05-24 15:57:53 +0800

    migrate to kotlin-dsl and set up for maven central

---
+++ buildSrc/src/main/kotlin/ApplicationConfiguration.kt
@@ -0,0 +1,10 @@
+object ApplicationConfiguration {
+    const val artifactId = "kotlin-lib-common-sdk"
+    const val groupId = "com.paysera.lib"
+    const val version = "2.6.1"
+    const val name = "Kotlin Library Common SDK"
+    const val url = "https://www.paysera.com/v2/en-GB/index"
+    const val description = "Paysera: Kotlin Common SDK Library"
+    const val releaseRepositoryUrl = "https://s01.oss.sonatype.org/content/repositories/releases/"
+    const val snapshotRepositoryUrl = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
+}
