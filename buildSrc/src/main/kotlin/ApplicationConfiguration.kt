76319137d58aae14112ec223fa6c03e10104bf80
Gilbert Parreno <gilbertparreno95@gmail.com>
2022-07-05 19:56:30 +0800

    remove snapshot

--- buildSrc/src/main/kotlin/ApplicationConfiguration.kt
+++ buildSrc/src/main/kotlin/ApplicationConfiguration.kt
@@ -1,7 +1,7 @@
 object ApplicationConfiguration {
     const val artifactId = "kotlin-lib-recurring-payments-sdk"
     const val groupId = "com.paysera.lib"
-    const val version = "1.8.7-SNAPSHOT"
+    const val version = "1.8.7"
     const val name = "Kotlin Library Recurring Payments SDK"
     const val url = "https://www.paysera.com/v2/en-GB/index"
     const val description = "Paysera: Kotlin Recurring Payments SDK Library"
