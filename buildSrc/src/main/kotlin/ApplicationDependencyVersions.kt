57edd7f18de3b9d1b8cb74d59f1d5ff23c5df28b
Gilbert Parreño <gilbert.parreno@paysera.net>
2023-07-03 15:07:04 +0800

    APP-5711: [Android] Failed to make Sepa transfer

--- buildSrc/src/main/kotlin/ApplicationDependencyVersions.kt
+++ buildSrc/src/main/kotlin/ApplicationDependencyVersions.kt
@@ -7,5 +7,5 @@ object ApplicationDependencyVersions {
     const val jodaMoney = "0.12"
     const val junit = "4.12"
     const val payseraCommonSdk = "2.6.9"
-    const val payseraAccountsSdk = "5.24.13"
+    const val payseraAccountsSdk = "5.24.15-SNAPSHOT"
 }
